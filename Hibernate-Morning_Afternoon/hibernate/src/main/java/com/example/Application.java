4eefecc16760aad03b54f3fcbd0b6b57fe782975
Hritik Chauhan <hritik.chauhan@accolitedigital.com>
2021-01-26 21:48:19 +0530

    Hibernate Assignment

---
+++ Hibernate-Morning_Afternoon/hibernate/src/main/java/com/example/Application.java
@@ -0,0 +1,115 @@
+package com.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.annotations.Cache;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class Application {
+    static void saveData(){
+        Alien a = new Alien();
+        a.setAid(2);
+        a.setAname("mkmmkn");
+        a.setColor("black");
+        Configuration con  = new Configuration().configure().addAnnotatedClass(Alien.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(a);
+        tx.commit();
+    }
+
+    static void getData(){
+        Alien a = new Alien();
+        Configuration con  = new Configuration().configure().addAnnotatedClass(Alien.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        a = (Alien) session.get(Alien.class, 1);
+        tx.commit();
+        System.out.println(a);
+    }
+
+    static void firstLevelCacheWorking(){
+        Alien a = new Alien();
+        Configuration con  = new Configuration().configure().addAnnotatedClass(Alien.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session1 = sf.openSession();
+        Transaction tx = session1.beginTransaction();
+        a = (Alien) session1.get(Alien.class, 1);
+        System.out.println(a);
+        a = (Alien) session1.get(Alien.class, 1);
+        System.out.println(a);
+        tx.commit();
+    }
+
+
+    static void firstLevelCache(){
+        Alien a = new Alien();
+        Configuration con  = new Configuration().configure().addAnnotatedClass(Alien.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session1 = sf.openSession();
+        Transaction tx1 = session1.beginTransaction();
+        a = (Alien) session1.get(Alien.class, 1);
+        System.out.println(a);
+        tx1.commit();
+        Session session2 = sf.openSession();
+        Transaction tx2 = session1.beginTransaction();
+        a = (Alien) session2.get(Alien.class, 1);
+        System.out.println(a);
+        tx2.commit();
+
+    }
+
+    public static void secondLevelCaching(){
+
+        System.out.println("Second Level Caching");
+        System.out.println("===Session 1===");
+        Configuration con  = new Configuration().configure().addAnnotatedClass(Alien.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session1 = sf.openSession();
+        Transaction tx1 = session1.beginTransaction();
+        Alien alien1 = (Alien)session1.get(Alien.class, 1001);
+        System.out.println(alien1);
+        tx1.commit();
+
+        System.out.println("===Session 2===");
+        Session session2 = sf.openSession();
+        Transaction tx2 = session2.beginTransaction();
+        alien1 = (Alien)session2.get(Alien.class, 1001);
+        System.out.println(alien1);
+        tx2.commit();
+
+    }
+
+    static void queryWise(){
+        Alien a = new Alien();
+        Configuration con  = new Configuration().configure().addAnnotatedClass(Alien.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session1 = sf.openSession();
+        Transaction tx1 = session1.beginTransaction();
+        Query q1 = session1.createQuery("from Alien where aid = 1");
+        q1.setCacheable(true);
+        a = (Alien) q1.uniqueResult();
+        System.out.println(a);
+        tx1.commit();
+        Session session2 = sf.openSession();
+        Transaction tx2 = session1.beginTransaction();
+        Query q2 = session2.createQuery("from Alien where aid = 1");
+        q2.setCacheable(true);
+        a = (Alien) q2.uniqueResult();
+        System.out.println(a);
+        tx2.commit();
+
+    }
+
+    public static void main(String [] args){
+        saveData();
+        getData();
+        firstLevelCacheWorking();
+        secondLevelCaching();
+        queryWise();
+    }
+}
